03b2d7f848f127c7689a0fdbcea285096b28696b
Anastasiia_Aghfir <anastasiia.aghfir@gmail.com>
2026-08-18 01:57:11 +0200

    fixed error

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -5,7 +5,7 @@ export const errorHandler = (err, req, res, next) => {
     return res.status(err.status).json({ message: err.message || err.name });
   }
 
-  const isProd = process.env.NODE_ENV === 'prodaction';
+  const isProd = process.env.NODE_ENV === 'production';
 
   res.status(500).json({
     message: isProd
